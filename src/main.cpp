ded6b28a368b160e5123809379ac174c38d106d8
mikitahap <mikitahap@outlook.com>
2025-10-29 12:02:46 +0100

    Fixed build issues with main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,31 @@
 #include <Eigen/Dense>
 #include "KalmanFilterR7.h"
 #include <fstream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main() {
-    CsvReader<float, float, float, float> barometrCsv("./out/barometer_clean.csv");
-    CsvReader<float, float, float, float> accelerometerCsv("./out/accelerometer_noisy_nosecone.csv");
-    std::ofstream file("output.txt");
-    file << "t, h, v\n";
+    try {
+        CsvReader<float, float, float, float> barometrCsv("data/barometer_clean.csv");
+        CsvReader<float, float, float, float> accelerometerCsv("data/accelerometer_noisy_nosecone.csv");
 
-    KalmanFilterR7 f(0.01);
-    for(int i =0; i < accelerometerCsv.rowCount(); i++) {
-        auto barometrRow = barometrCsv[i/2]; 
-        auto accelerometerRow = accelerometerCsv[i]; 
-        auto r = f.processMeasurement(accelerometerRow.get<1>(), barometrRow.get<1>()); 
-        file << accelerometerRow.get<0>() << ", " << r(0) << ", " << r(1) << "\n"; 
+        std::ofstream file("output.txt");
+        file << "t, h, v\n";
+
+        KalmanFilterR7 f(0.01);
+        for(int i =0; i < accelerometerCsv.rowCount(); i++) {
+            auto barometrRow = barometrCsv[i/2]; 
+            auto accelerometerRow = accelerometerCsv[i]; 
+            auto r = f.processMeasurement(accelerometerRow.get<1>(), barometrRow.get<1>()); 
+            file << accelerometerRow.get<0>() << ", " << r(0) << ", " << r(1) << "\n"; 
+        }
+       
+        file.close();
+    } catch (const std::exception& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        return 1;
     }
-
-   
-    file.close();
+    
+    return 0;
 }
